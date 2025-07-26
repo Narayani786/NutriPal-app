687ecdf9d33768c411a6280e5e09106d892ab01d
Saloni <saloninarayana1@gmail.com>
2025-07-26 10:04:37 +0530

    added main logic of submition

---
+++ nutripal/src/components/FoodInput.jsx
@@ -0,0 +1,36 @@
+import React, { use, useState } from 'react';
+
+const FoodInput = ({ onAdd }) => {
+    const [food, setFood] = useState('');
+    const [calories, setCalories] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if(!food || !calories) return;
+        onAdd({ name: food, calories: Number(calories) });
+        setFood('');
+        setCalories('');
+    };
+
+    return (
+        <form onSubmit={handleSubmit} className='input-form'>
+            <input
+            type='text'
+            placeholder='Food'
+            value={food}
+            onChange={(e) => setFood(e.target.value)}
+            />
+
+            <input
+            type='number'
+            placeholder='Calories'
+            value={calories}
+            onChange={(e) => setCalories(e.target.value)}
+            />
+
+            <button type="submit">Add</button>
+        </form>
+    );
+};
+
+export default FoodInput;
