789730ba015d0a150755b7e9b7eb7eaae1107e45
Saloni <saloninarayana1@gmail.com>
2025-07-26 10:22:38 +0530

    Summed the logic of calorie's increment or decrement as per yesterday

---
+++ nutripal/src/components/SummaryBox.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const SummaryBox = ({ todalTotal, yesterdayTotal }) => {
+    const compare = yesterdayTotal !== null? todalTotal - yesterdayTotal : null;
+
+    return (
+        <div className='summary-box'>
+            <p>
+                <strong>📆 Today:</strong> {new Date().toDateString()}
+            </p>
+            <p>
+                <strong>📊 Today:</strong> {todalTotal} kcal
+            </p>
+            {compare !== null && (
+                <p>
+                    🔁 Compared to yesterday:{' '}
+                    <span className={compare > 0 ? "red" : "green"}>
+                        {compare > 0 ? `↑ ${compare}` : `↓ ${-compare}`} kcal
+                    </span>
+                </p>
+            )}
+        </div>
+    );
+};
+
+export default SummaryBox;
