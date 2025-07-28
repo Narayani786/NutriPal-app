d6074d1254b37d8f715265e23a439487a00bf57b
Saloni <saloninarayana1@gmail.com>
2025-07-28 08:05:57 +0530

    added deletion logic

--- nutripal/src/components/FoodList.jsx
+++ nutripal/src/components/FoodList.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
 
-const FoodList = ({ foodList }) => {
+const FoodList = ({ foodList, setFoodList }) => {
+
+    const handleDelete = (id) => {
+        const updateList = foodList.filter(item => item.id !== id);
+        setFoodList(updateList);
+
+        const today = new Date().toISOString().split('T')[0];
+        localStorage.setItem(today, JSON.stringify(updateList));
+    };
+
     return (
-        <ul className='food-list'>
-            {foodList.map((item, index) => (
-                <li key={index}>
-                    🍽️{item.name} - {item.calories} kcal
-                </li>
-            ))}
-        </ul>
+       <div className='food-list'>
+        <h3>Your Food Entries</h3>
+        {foodList.length === 0 ? (
+            <p>No food added yet today.</p>
+        ) : (
+            foodList.map(item => (
+                <div key={item.id} className='food-item'>
+                    <span>{item.name} - {item.calories} cal</span>
+                    <button className='delete-btn' onClick={() => handleDelete(item.id)}>✕</button>
+                </div >
+            ))
+        )}
+       </div>
     );
 };
 
